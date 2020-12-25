ddb224738e418df8d5da3e9089fa01cda45a4a45
Alex Pescaru <apescaru11@gmail.com>
2020-12-25 16:37:29 +0200

    ceva din firebase...

--- app/src/main/java/ro/poli/sma/proiectsma/FirebaseInterface.java
+++ app/src/main/java/ro/poli/sma/proiectsma/FirebaseInterface.java
@@ -1,28 +1,48 @@
 package ro.poli.sma.proiectsma;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.List;
 
 import ro.poli.sma.proiectsma.Exceptions.NicknameAlreadyUsed;
 
 public class FirebaseInterface {
+    private DatabaseReference dbRef;
+    private DatabaseReference thisRef;
+
+    public void init(String crtUsr){
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        thisRef = dbRef.child(crtUsr);
+    }
 
     public void register(PlayerInfo crtPlayerInfo){
 
     }
 
     public void setNickname(String nickname) throws NicknameAlreadyUsed{
-
+        thisRef.child("nickname").setValue(nickname);
     }
 
     public void login(PlayerInfo crtPlayerInfo){
 
     }
 
-    public List<Score> getScores(){
+    public Score getScores(){
         return null;
     }
 
+    private void getValues(){
+
+    }
+
     public void putScore(Score score){
+
 
     }
 
